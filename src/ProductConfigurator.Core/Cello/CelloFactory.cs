8094fd429570fa38b257b6f69d946ae5e7dd4517
jcl86 <jorgecano86@gmail.com>
2021-05-13 21:17:25 +0200

    Working

--- src/ProductConfigurator.Core/Cello/CelloFactory.cs
+++ src/ProductConfigurator.Core/Cello/CelloFactory.cs
@@ -4,15 +4,17 @@ namespace ProductConfigurator.Core
 {
     public static class CelloFactory
     {
-        public static Item Linen => Item.Create("Linen", "desc linen", 1000m);
-        public static Item LinenNatural => Item.Create("Line natural", "desc linen", 1000m);
+        public static Item Title => Item.Create("Eco linen Cello case", "Technological craftsmanship in a natural linen cello case that protects your instrument and your back forever.", 1160m);
+        public static Item Linen => Item.Create("Natural linen", @"Linen is a surly looking fabric, like sackcloth.
+We harden the fabric with the same process used to harden carbon fibre, until the surface is rigid.Its appearance is original, different, and homely.
+The linen composite is used in high end designer furniture.", 1160m);
 
-        public static Item Milk => Item.Create("Milk", "desc milk", 1m);
-        public static Item Diagonalink => Item.Create("Diagonalink", "desc diagonalink", 1m);
-        public static Item Duplo => Item.Create("Duplo", "desc duplo", 1m);
-        public static Item NoColor => Item.Create("No color", "", 0m);
+        public static Item Milk => Item.Create("Milk", "A smooth design for a sleek  look.", 100m); //RAL orange:2002, marine blue:5013, yellow:1021, pink: 4003
+        public static Item Diagonalink => Item.Create("Diagonalink", "An elegant twist on the DUPLO collection.", 120m);//yellow, black, blue, rosie
+        public static Item Duplo => Item.Create("Duplo", "A Bolder Version of the MILK collection for a a more exaggerated statement.", 100m);//blue, yellow, red, pink, nude, classic
+        public static Item Natural => Item.Create("Natural", "New fresh Handmade style", 0m);
 
-        public static IEnumerable<Item> Materials => new List<Item>() { Linen, LinenNatural }; 
-        public static IEnumerable<Item> Colors => new List<Item>() { Milk, Diagonalink, Duplo, NoColor }; 
+        public static IEnumerable<Item> Materials => new List<Item>() { Linen }; 
+        public static IEnumerable<Item> Colors => new List<Item>() { Milk, Diagonalink, Duplo, Natural }; 
     }
 }
