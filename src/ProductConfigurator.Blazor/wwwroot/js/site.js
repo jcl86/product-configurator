3771e93698cce9140b1cc897c8000b1100017f3f
jcl86 <jorgecano86@gmail.com>
2021-06-29 22:19:27 +0200

    #2 Send mail with js

---
+++ src/ProductConfigurator.Blazor/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿window.blazorExtensions = {
+    SendLocalEmail: function (mailto, subject, body) {
+        var link = document.createElement('a');
+        var uri = "mailto:" + mailto + "?";
+        if (!isEmpty(subject)) {
+            uri = uri + "subject=" + subject;
+        }
+
+        if (!isEmpty(body)) {
+            if (!isEmpty(subject)) { // We already appended one querystring parameter, add the '&' separator
+                uri = uri + "&"
+            }
+
+            uri = uri + "body=" + body;
+        }
+
+        uri = encodeURI(uri);
+        uri = uri.substring(0, 2000); // Avoid exceeding querystring limits.
+        console.log('Clicking SendLocalEmail link:', uri);
+
+        link.href = uri;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    }
+};
+
+function isEmpty(str) {
+    return (!str || str.length === 0);
+}
